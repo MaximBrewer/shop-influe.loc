97063549ba80012ab8c087371ada24eb2e47904e
MaximBrewer <pimax1978@icloud.com>
2023-10-17 14:44:52 +0600

    sdf

--- public/build/assets/Top-9790c35d.js
+++
@@ -1,1 +0,0 @@
-import{q as r,a as e}from"./app-ab16fca2.js";import{B as s}from"./Breadcrumbs-87b968d7.js";const i=()=>{const a=r().props;return e("div",{className:"catalogue-categories",children:e("div",{className:"container-outer",children:e("div",{className:"catalogue-categories__outer",children:e("div",{className:"catalogue-categories__inner",children:e(s,{...a})})})})})};export{i as T};
